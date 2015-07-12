f41940aec6d59ad7c68bcaafd0e9b82e3c835565
Arthur Clemens <arthurclemens@gmail.com>
2015-07-12 22:49:30 +0200

    Tweak build script

--- examples/src/config-build.js
+++ examples/src/config-build.js
@@ -4,12 +4,5 @@ System.config({
         '*': '*.js',
         '*.css': '*.css',
         '*.svg': '*.svg'
-    },
-    'map': {
-        'mithril-infinite': 'lib/mithril-infinite/mithril-infinite',
-        'verge': 'lib/verge/verge.min',
-        'mithril': 'lib/mithril/mithril.min',
-        'css': 'lib/system-css/css',
-        'text': 'lib/system-text/text'
     }
 });
